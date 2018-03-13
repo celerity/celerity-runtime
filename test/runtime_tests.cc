254794cc86865252352c96f2fba89bf8300f1de1
Florian Tischler <florian.tischler@outlook.com>
2018-03-13 16:34:09 +0100

    ported runtime tests to catch2

--- test/runtime_tests.cc
+++ test/runtime_tests.cc
@@ -1,41 +1,43 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include <catch.hpp>
 #include <celerity_runtime.h>
 
 namespace celerity {
-TEST(buffer_state, Basic) {
+TEST_CASE("Basic") {
   detail::buffer_state<1> bs(cl::sycl::range<1>(256), 2);
-  EXPECT_EQ(bs.get_dimensions(), 1);
+  REQUIRE( bs.get_dimensions() == 1);
 
   auto sn = bs.get_source_nodes(GridRegion<1>(256));
-  EXPECT_EQ(sn.size(), 1);
-  EXPECT_EQ(sn[0].first, GridBox<1>(256));
-  EXPECT_EQ(sn[0].second.size(), 2);
-  EXPECT_EQ(sn[0].second.count(0), 1);
-  EXPECT_EQ(sn[0].second.count(1), 1);
+  REQUIRE(sn.size() == 1);
+  REQUIRE(sn[0].first == GridBox<1>(256));
+  REQUIRE(sn[0].second.size() == 2);
+  REQUIRE(sn[0].second.count(0) == 1);
+  REQUIRE(sn[0].second.count(1) == 1);
 }
 
-TEST(buffer_state, UpdateRegion) {
+TEST_CASE("UpdateRegion") {
   detail::buffer_state<1> bs(cl::sycl::range<1>(256), 2);
   bs.update_region({0, 128}, {1});
 
   auto sn = bs.get_source_nodes(GridRegion<1>(32, 64));
-  EXPECT_EQ(sn.size(), 1);
-  EXPECT_EQ(sn[0].first, GridBox<1>(32, 64));
-  EXPECT_EQ(sn[0].second.size(), 1);
-  EXPECT_EQ(sn[0].second.count(1), 1);
+  REQUIRE(sn.size() == 1);
+  REQUIRE(sn[0].first == GridBox<1>(32, 64));
+  REQUIRE(sn[0].second.size() == 1);
+  REQUIRE(sn[0].second.count(1) == 1);
 
   sn = bs.get_source_nodes(GridRegion<1>(256));
-  EXPECT_EQ(sn.size(), 2);
-  EXPECT_EQ(sn[0].first, GridBox<1>(128, 256));
-  EXPECT_EQ(sn[0].second.size(), 2);
-  EXPECT_EQ(sn[0].second.count(0), 1);
-  EXPECT_EQ(sn[0].second.count(1), 1);
-  EXPECT_EQ(sn[1].first, GridBox<1>(0, 128));
-  EXPECT_EQ(sn[1].second.size(), 1);
-  EXPECT_EQ(sn[1].second.count(1), 1);
+  REQUIRE(sn.size() == 2);
+  REQUIRE(sn[0].first == GridBox<1>(128, 256));
+  REQUIRE(sn[0].second.size() == 2);
+  REQUIRE(sn[0].second.count(0) == 1);
+  REQUIRE(sn[0].second.count(1) == 1);
+  REQUIRE(sn[1].first == GridBox<1>(0, 128));
+  REQUIRE(sn[1].second.size() == 1);
+  REQUIRE(sn[1].second.count(1) == 1);
 }
 
-TEST(buffer_state, CollapseRegions) {
+TEST_CASE("CollapseRegions") {
   // We test buffer_state<>::collapse_regions by observing the order of the
   // returned boxes. This somewhat relies on implementation details of
   // buffer_state<>::get_source_nodes.
@@ -45,20 +47,20 @@ TEST(buffer_state, CollapseRegions) {
   bs.update_region({192, 256}, {1});
 
   auto sn = bs.get_source_nodes(GridRegion<1>(64, 256));
-  EXPECT_EQ(sn.size(), 3);
-  EXPECT_EQ(sn[0].first, GridBox<1>(64, 128));
-  EXPECT_EQ(sn[0].second.size(), 1);
-  EXPECT_EQ(sn[0].second.count(1), 1);
+  REQUIRE(sn.size() == 3);
+  REQUIRE(sn[0].first == GridBox<1>(64, 128));
+  REQUIRE(sn[0].second.size() == 1);
+  REQUIRE(sn[0].second.count(1) == 1);
 
   // Since this one is returned before the [128,192) box,
   // the {[64,128), [192,256)} region must exist internally.
-  EXPECT_EQ(sn[1].first, GridBox<1>(192, 256));
-  EXPECT_EQ(sn[1].second.size(), 1);
-  EXPECT_EQ(sn[1].second.count(1), 1);
+  REQUIRE(sn[1].first == GridBox<1>(192, 256));
+  REQUIRE(sn[1].second.size() == 1);
+  REQUIRE(sn[1].second.count(1) == 1);
 
-  EXPECT_EQ(sn[2].first, GridBox<1>(128, 192));
-  EXPECT_EQ(sn[2].second.size(), 2);
-  EXPECT_EQ(sn[2].second.count(0), 1);
-  EXPECT_EQ(sn[2].second.count(1), 1);
+  REQUIRE(sn[2].first == GridBox<1>(128, 192));
+  REQUIRE(sn[2].second.size() == 2);
+  REQUIRE(sn[2].second.count(0) == 1);
+  REQUIRE(sn[2].second.count(1) == 1);
 }
 }  // namespace celerity
