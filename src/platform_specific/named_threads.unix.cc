ec7f0d26a858590335dfdfdb47082555f79d896e
Markus Wippler <markus.wippler@uibk.ac.at>
2022-02-21 16:45:00 +0100

    Implement named threads for unix

--- src/platform_specific/named_threads.unix.cc
+++ src/platform_specific/named_threads.unix.cc
@@ -1,13 +1,44 @@
 #include "named_threads.h"
 
+#include <cassert>
+#include <type_traits>
+
+#include <pthread.h>
+
 namespace celerity::detail {
 
-void set_thread_name(std::thread& thread, const std::string& name) {}
+static_assert(std::is_same_v<std::thread::native_handle_type, pthread_t>, "Unexpected native thread handle type");
 
-void set_current_thread_name(const std::string& name) {}
+constexpr auto PTHREAD_MAX_THREAD_NAME_LEN = 16;
 
-std::string get_thread_name(std::thread& thread) {}
+static inline void set_thread_name_unix(const pthread_t thread_handle, const std::string& name) {
+	auto truncated_name = name;
+	truncated_name.resize(PTHREAD_MAX_THREAD_NAME_LEN - 1); // -1 because of null terminator
+	const auto res = pthread_setname_np(thread_handle, truncated_name.c_str());
+	assert(res == 0 && "Failed to set thread name");
+}
 
-std::string get_current_thread_name() {}
+static inline std::string get_thread_name_unix(const pthread_t thread_handle) {
+	auto name = std::string(PTHREAD_MAX_THREAD_NAME_LEN, '\0');
+	const auto res = pthread_getname_np(thread_handle, name.data(), name.size());
+	assert(res == 0 && "Failed to get thread name");
+	return name.c_str(); // Strip null terminator
+}
+
+void set_thread_name(std::thread& thread, const std::string& name) {
+	set_thread_name_unix(thread.native_handle(), name);
+}
+
+void set_current_thread_name(const std::string& name) {
+	set_thread_name_unix(pthread_self(), name);
+}
+
+std::string get_thread_name(std::thread& thread) {
+	return get_thread_name_unix(thread.native_handle());
+}
+
+std::string get_current_thread_name() {
+	return get_thread_name_unix(pthread_self());
+}
 
 } // namespace celerity::detail
