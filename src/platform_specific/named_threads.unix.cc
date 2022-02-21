abad55bf2ef7450807db1e0eb3a02dca12b0a654
Markus Wippler <markus.wippler@uibk.ac.at>
2022-02-21 16:55:28 +0100

    Fix unused variable warning in release builds

--- src/platform_specific/named_threads.unix.cc
+++ src/platform_specific/named_threads.unix.cc
@@ -14,13 +14,13 @@ constexpr auto PTHREAD_MAX_THREAD_NAME_LEN = 16;
 static inline void set_thread_name_unix(const pthread_t thread_handle, const std::string& name) {
 	auto truncated_name = name;
 	truncated_name.resize(PTHREAD_MAX_THREAD_NAME_LEN - 1); // -1 because of null terminator
-	const auto res = pthread_setname_np(thread_handle, truncated_name.c_str());
+	[[maybe_unused]] const auto res = pthread_setname_np(thread_handle, truncated_name.c_str());
 	assert(res == 0 && "Failed to set thread name");
 }
 
 static inline std::string get_thread_name_unix(const pthread_t thread_handle) {
 	auto name = std::string(PTHREAD_MAX_THREAD_NAME_LEN, '\0');
-	const auto res = pthread_getname_np(thread_handle, name.data(), name.size());
+	[[maybe_unused]] const auto res = pthread_getname_np(thread_handle, name.data(), name.size());
 	assert(res == 0 && "Failed to get thread name");
 	return name.c_str(); // Strip null terminator
 }
