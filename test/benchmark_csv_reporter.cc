650f312cc1b7f426965422440b768af09adb0a01
Philip Salzmann <philip.salzmann@uibk.ac.at>
2022-03-29 15:46:21 +0200

    Improve benchmark CSV reporter
    - Include raw samples in output
    - Don't print part number for distinguishing generator values, instead
      detect uses of GENERATE and print a warning.

--- test/benchmark_csv_reporter.cc
+++ test/benchmark_csv_reporter.cc
@@ -18,6 +18,42 @@ static std::string escape_csv(const std::string& str) {
 	return fmt::format("\"{}\"", std::regex_replace(str, re, "\"\""));
 }
 
+class benchmark_reporter_base : public Catch::StreamingReporterBase {
+  public:
+	using StreamingReporterBase::StreamingReporterBase;
+
+	// TODO: Do we want to somehow report this?
+	void benchmarkFailed(Catch::StringRef benchmarkName) override { StreamingReporterBase::benchmarkFailed(benchmarkName); }
+
+	void sectionStarting(Catch::SectionInfo const& sectionInfo) override {
+		StreamingReporterBase::sectionStarting(sectionInfo);
+		// Each test case has an implicit section with the name of the test case itself,
+		// so there is no need to capture that separately.
+		active_sections.push_back(sectionInfo.name);
+	}
+
+	void testCasePartialEnded(Catch::TestCaseStats const& testCaseStats, uint64_t partNumber) override {
+		StreamingReporterBase::testCasePartialEnded(testCaseStats, partNumber);
+		// If the exact same set of sections was active as before, generators must have been involved.
+		if(active_sections == previous_active_sections) {
+			if(!did_print_generators_warning) {
+				fmt::print("WARNING: Using generators will result in indistinguishable test case columns.\n");
+				did_print_generators_warning = true;
+			}
+		}
+		std::swap(active_sections, previous_active_sections);
+		active_sections.clear();
+	}
+
+  protected:
+	std::string get_test_case_name() const { return fmt::format("{}", fmt::join(active_sections, " > ")); }
+
+  private:
+	std::vector<std::string> active_sections;
+	std::vector<std::string> previous_active_sections;
+	bool did_print_generators_warning = false;
+};
+
 /**
  * Prints benchmark results in CSV format.
  * All timings are in nanoseconds.
@@ -26,62 +62,32 @@ static std::string escape_csv(const std::string& str) {
  * active in the current invocation will be printed as well (not just those directly
  * surrounding a BENCHMARK).
  */
-class benchmark_csv_reporter : public Catch::StreamingReporterBase {
+class benchmark_csv_reporter : public benchmark_reporter_base {
   public:
-	using StreamingReporterBase::StreamingReporterBase;
+	using benchmark_reporter_base::benchmark_reporter_base;
 
 	static std::string getDescription() { return "Reporter for benchmarks in CSV format"; }
 
 	void testRunStarting(Catch::TestRunInfo const& testRunInfo) override {
 		StreamingReporterBase::testRunStarting(testRunInfo);
-		fmt::print(m_stream, "test case,part,benchmark name,samples,iterations,estimated,mean,low mean,high mean,std dev,low std dev,high std dev\n");
-	}
-
-	void benchmarkStarting(Catch::BenchmarkInfo const& benchmarkInfo) override {
-		StreamingReporterBase::benchmarkStarting(benchmarkInfo);
-		fmt::print(m_stream, "{},{},{},{},{},{},", fmt::join(active_sections, " > "), active_part, escape_csv(benchmarkInfo.name), benchmarkInfo.samples,
-		    benchmarkInfo.iterations, benchmarkInfo.estimatedDuration);
+		fmt::print(m_stream, "test case,benchmark name,samples,iterations,estimated,mean,low mean,high mean,std dev,low std dev,high std dev,raw\n");
 	}
 
 	void benchmarkEnded(Catch::BenchmarkStats<> const& benchmarkStats) override {
 		StreamingReporterBase::benchmarkEnded(benchmarkStats);
-		fmt::print(m_stream, "{:.4f},{:.4f},{:.4f},{:.4f},{:.4f},{:.4f}\n", benchmarkStats.mean.point.count(), benchmarkStats.mean.lower_bound.count(),
+		auto& info = benchmarkStats.info;
+		fmt::print(m_stream, "{},{},{},{},{},", escape_csv(get_test_case_name()), escape_csv(info.name), info.samples, info.iterations, info.estimatedDuration);
+		fmt::print(m_stream, "{:.4f},{:.4f},{:.4f},{:.4f},{:.4f},{:.4f},", benchmarkStats.mean.point.count(), benchmarkStats.mean.lower_bound.count(),
 		    benchmarkStats.mean.upper_bound.count(), benchmarkStats.standardDeviation.point.count(), benchmarkStats.standardDeviation.lower_bound.count(),
 		    benchmarkStats.standardDeviation.upper_bound.count());
+		// Finally print all raw values for custom analyses (as quoted comma-separated values).
+		std::vector<double> raw;
+		raw.reserve(benchmarkStats.samples.size());
+		std::transform(benchmarkStats.samples.cbegin(), benchmarkStats.samples.cend(), std::back_inserter(raw), [](auto& d) { return d.count(); });
+		fmt::print(m_stream, "\"{:.4f}\"\n", fmt::join(raw, ","));
 		// Flush so we can watch results come in when writing to file
 		m_stream.flush();
 	}
-
-	// It looks like benchmarkStarting is not called in case a benchmark fails, so we don't need to print the remaining columns either.
-	// TODO: Do we want to somehow report this anyway?
-	void benchmarkFailed(Catch::StringRef benchmarkName) override { StreamingReporterBase::benchmarkFailed(benchmarkName); }
-
-	void testCasePartialStarting(Catch::TestCaseInfo const& testInfo, uint64_t partNumber) override {
-		StreamingReporterBase::testCasePartialStarting(testInfo, partNumber);
-		active_part = partNumber;
-	}
-
-	void sectionStarting(Catch::SectionInfo const& sectionInfo) override {
-		StreamingReporterBase::sectionStarting(sectionInfo);
-		// Each test case has an implicit section with the name of the test case itself,
-		// so there is no need to capture that separately.
-		active_sections.push_back(escape_csv(sectionInfo.name));
-	}
-
-	void testCasePartialEnded(Catch::TestCaseStats const& testCaseStats, uint64_t partNumber) override {
-		StreamingReporterBase::testCasePartialEnded(testCaseStats, partNumber);
-		active_part = 0;
-		active_sections.clear();
-	}
-
-  private:
-	// It is difficult to distinguish test case invocations for different generator values, since
-	// as of Catch2 v.3.0.0-preview4 there is unfortunately no real metadata available (for example,
-	// it is not possible to get the current value returned for each of the generators).
-	// The only piece of information we have is the "part number", which we emit to distinguish
-	// different generator values. Note however that sections also increment the part number.
-	uint64_t active_part;
-	std::vector<std::string> active_sections;
 };
 
 CATCH_REGISTER_REPORTER("celerity-benchmark-csv", benchmark_csv_reporter)
