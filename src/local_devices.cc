93704a0a73d7169f15ac6b1cf1c7e964306a626f
Philip Salzmann <philip.salzmann@uibk.ac.at>
2022-11-09 17:03:18 +0100

    [MGPU] Add "legacy mode": One device per rank

--- src/local_devices.cc
+++ src/local_devices.cc
@@ -6,6 +6,15 @@ void local_devices::init(const config& cfg) {
 	assert(!m_is_initialized);
 	m_is_initialized = true;
 
+	if(cfg.get_host_config().node_count > 1) {
+		CELERITY_WARN("Celerity detected more than one MPI rank on this host. Running in legacy mode (one device per rank).");
+		sycl::device legacy_mode_device = celerity::detail::pick_device(cfg, auto_select_device{}, sycl::platform::get_platforms());
+
+		m_device_queues.emplace_back(0, get_memory_id(0));
+		m_device_queues.back().init(cfg, legacy_mode_device);
+		return;
+	}
+
 	// NOCOMMIT We're simply selecting all GPUs for now
 	const auto all_devices = sycl::device::get_devices(sycl::info::device_type::gpu);
 	for(device_id did = 0; did < all_devices.size(); ++did) {
