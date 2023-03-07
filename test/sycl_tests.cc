3f0a1491f9e87c5916507cceb532f298a9ca3d1d
Philip Salzmann <philip.salzmann@uibk.ac.at>
2023-03-07 17:36:12 +0100

    Use native pointers for backing buffer allocations
    Use native pointers (allocated using `sycl::malloc_device`) instead of
    relying on SYCL buffers for backing Celerity virtual buffers.
    
    This greatly simplifies various aspects of accessors and buffer
    management while enabling future optimizations. Futhermore, by using
    native pointers we completely circumvent any dataflow analysis performed
    by the SYCL runtime.

--- test/sycl_tests.cc
+++ test/sycl_tests.cc
@@ -8,64 +8,6 @@ namespace celerity::detail {
 template <int Dims>
 class dim_device_queue_fixture : public test_utils::device_queue_fixture {};
 
-template <int Dims>
-class accessor_range_test_kernel;
-
-TEMPLATE_TEST_CASE_METHOD_SIG(dim_device_queue_fixture, "ranged_sycl_access works around differences in offset computation for device accessors",
-    "[sycl][accessor]", ((int Dims), Dims), 1, 2, 3) {
-	constexpr static size_t tile_width = 2;
-	constexpr static size_t num_tiles[] = {2, 3, 4};
-
-	const auto tile_range = range_cast<Dims>(sycl::range<3>{tile_width, tile_width, tile_width});
-	const auto buffer_range = range_cast<Dims>(sycl::range<3>{2 * tile_width, 3 * tile_width, 4 * tile_width});
-
-	// GENERATE macros cannot be executed in a loop
-	sycl::id<Dims> offset;
-	if constexpr(Dims >= 1) { offset[0] = GENERATE(Catch::Generators::range(size_t{0}, num_tiles[0])) * tile_width; }
-	if constexpr(Dims >= 2) { offset[1] = GENERATE(Catch::Generators::range(size_t{0}, num_tiles[1])) * tile_width; }
-	if constexpr(Dims >= 3) { offset[2] = GENERATE(Catch::Generators::range(size_t{0}, num_tiles[2])) * tile_width; }
-
-	CAPTURE(buffer_range);
-	CAPTURE(tile_range);
-	CAPTURE(offset);
-
-	auto& q = dim_device_queue_fixture<Dims>::get_device_queue();
-
-	sycl::buffer<int, Dims> buf{buffer_range};
-	q.submit([&](sycl::handler& cgh) { cgh.fill(buf.template get_access<sycl::access_mode::discard_write>(cgh), -1); });
-
-	q.submit([&](sycl::handler& cgh) {
-		const auto acc = buf.template get_access<sycl::access_mode::read_write>(cgh, tile_range, offset);
-		const auto buf_range = buf.get_range();
-		cgh.parallel_for<bind_kernel_name<accessor_range_test_kernel<Dims>>>(tile_range, [=](const sycl::id<Dims> rel_index) {
-			const auto abs_index = offset + rel_index;
-			int value = 0;
-			for(int d = 0; d < Dims; ++d) {
-				value = 10 * value + static_cast<int>(abs_index[d]);
-			}
-			ranged_sycl_access(acc, buf_range, rel_index) += 1 + value;
-		});
-	});
-
-	std::vector<int> expected;
-	expected.reserve(buffer_range.size());
-	test_utils::for_each_in_range(buffer_range, [&](const id<Dims> index) {
-		bool inside_tile = true;
-		int value = 0;
-		for(int d = 0; d < Dims; ++d) {
-			inside_tile &= index[d] >= offset[d] && index[d] < offset[d] + tile_width;
-			value = 10 * value + static_cast<int>(index[d]);
-		}
-		expected.push_back(inside_tile ? value : -1);
-	});
-
-	std::vector<int> actual(buf.get_range().size());
-	q.submit([&](sycl::handler& cgh) { cgh.copy(buf.template get_access<sycl::access::mode::read>(cgh), actual.data()); }).wait_and_throw();
-
-	CHECK(actual == expected);
-}
-
-
 template <access_mode, bool>
 class access_test_kernel;
 
@@ -83,68 +25,6 @@ static auto make_device_accessor(sycl::buffer<int, 1>& buf, sycl::handler& cgh, 
 		return acc;
 	} else {
 		return buf.get_access<AccessMode>(cgh, sr.range, sr.offset);
-	}
-}
-
-template <access_mode AccessMode, bool UsingPlaceholderAccessor>
-static void test_access(sycl::queue& q, sycl::buffer<int, 1>& test_buf, const subrange<1>& sr) {
-	CAPTURE(AccessMode);
-	CAPTURE(UsingPlaceholderAccessor);
-
-	bool verified = false;
-	sycl::buffer<bool> verify_buf{&verified, 1};
-	q.submit([&](sycl::handler& cgh) {
-		 const auto test_acc = make_device_accessor<AccessMode, UsingPlaceholderAccessor>(test_buf, cgh, sr);
-		 const auto verify_acc = verify_buf.get_access<access_mode::write>(cgh);
-		 cgh.parallel_for<access_test_kernel<AccessMode, UsingPlaceholderAccessor>>(range<1>{1}, [=](sycl::item<1>) { //
-			 verify_acc[0] = test_acc.get_range() == sr.range;
-		 });
-	 }).wait_and_throw();
-
-	sycl::host_accessor verify_acc{verify_buf};
-	CHECK(verify_acc[0]);
-};
-
-// We artificially allocate unit-sized SYCL buffers for ComputeCpp and DPC++ to work around the lack of zero-sized accessor support. Once this test succeeds,
-// we can get rid of the workaround.
-TEST_CASE_METHOD(test_utils::device_queue_fixture, "SYCL can access empty buffers natively", "[sycl][accessor][!mayfail]") {
-	sycl::buffer<int> buf{zero_range};
-
-	auto& queue = get_device_queue().get_sycl_queue();
-	const auto sr = subrange<1>{0, 0};
-
-	SECTION("Using regular accessors") {
-		test_access<access_mode::discard_write, false>(queue, buf, sr);
-		test_access<access_mode::read_write, false>(queue, buf, sr);
-		test_access<access_mode::read, false>(queue, buf, sr);
-	}
-
-	SECTION("Using placeholder accessors") {
-		test_access<access_mode::discard_write, true>(queue, buf, sr);
-		test_access<access_mode::read_write, true>(queue, buf, sr);
-		test_access<access_mode::read, true>(queue, buf, sr);
-	}
-}
-
-TEST_CASE_METHOD(test_utils::device_queue_fixture, "SYCL can access empty buffers through device_buffer_storage", "[sycl][accessor]") {
-	detail::device_buffer_storage<int, 1> storage{zero_range, get_device_queue().get_sycl_queue()};
-	CHECK(range_cast<1>(storage.get_range()) == zero_range);
-
-	auto& queue = get_device_queue().get_sycl_queue();
-	auto& buf = storage.get_device_buffer();
-	const auto requested_sr = subrange<1>{buf.get_range()[0], 0}; // offset == backing buffer range just to mess with things
-	const auto effective_sr = detail::get_effective_sycl_accessor_subrange({}, requested_sr);
-
-	SECTION("Using regular accessors") {
-		test_access<access_mode::discard_write, false>(queue, buf, effective_sr);
-		test_access<access_mode::read_write, false>(queue, buf, effective_sr);
-		test_access<access_mode::read, false>(queue, buf, effective_sr);
-	}
-
-	SECTION("Using placeholder accessors") {
-		test_access<access_mode::discard_write, true>(queue, buf, effective_sr);
-		test_access<access_mode::read_write, true>(queue, buf, effective_sr);
-		test_access<access_mode::read, true>(queue, buf, effective_sr);
 	}
 }
 
