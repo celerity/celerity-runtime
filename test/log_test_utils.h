ebb68e734ecf44c151985fa790d8ee66b1da778b
PeterTh <petert@dps.uibk.ac.at>
2023-06-27 10:52:22 +0200

    Fix a sporadic lifetime issue with log capturing in unit tests (#185)

--- test/log_test_utils.h
+++ test/log_test_utils.h
@@ -16,11 +16,15 @@ class log_capture {
 		m_level_before = spdlog::get_level();
 		m_tmp_level = std::min(m_level_before, level);
 		spdlog::set_level(m_tmp_level);
+		m_flush_before = logger->flush_level();
+		m_tmp_flush = std::min(m_flush_before, level);
+		logger->flush_on(m_tmp_flush);
 	}
 
 	~log_capture() {
 		spdlog::set_level(m_level_before);
 		const auto logger = spdlog::default_logger_raw();
+		logger->flush_on(m_flush_before);
 		assert(*logger->sinks().rbegin() == m_ostream_sink);
 		logger->sinks().pop_back();
 	}
@@ -31,7 +35,7 @@ class log_capture {
 	}
 
   private:
-	spdlog::level::level_enum m_tmp_level, m_level_before;
+	spdlog::level::level_enum m_tmp_level, m_level_before, m_tmp_flush, m_flush_before;
 	std::ostringstream m_oss;
 	std::shared_ptr<spdlog::sinks::ostream_sink_st> m_ostream_sink;
 
@@ -39,6 +43,10 @@ class log_capture {
 		if(spdlog::get_level() != m_tmp_level) {
 			FAIL_CHECK("global spdlog level has changed since log_capture was instantiated - captured logs may depend on CELERITY_LOG_LEVEL!");
 			m_tmp_level = spdlog::get_level();
+		}
+		if(spdlog::default_logger_raw()->flush_level() != m_tmp_flush) {
+			FAIL_CHECK("spdlog flush level has changed since log_capture was instantiated!");
+			m_tmp_flush = spdlog::default_logger_raw()->flush_level();
 		}
 	}
 };
