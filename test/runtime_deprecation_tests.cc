668ace7078485f1be849ee0a9b5e6d008ac224aa
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2023-07-11 17:27:05 +0200

    Remove deprecated template arguments from built-in range mappers

--- test/runtime_deprecation_tests.cc
+++ test/runtime_deprecation_tests.cc
@@ -18,37 +18,6 @@
 namespace celerity {
 namespace detail {
 
-	using celerity::access::all;
-	using celerity::access::fixed;
-	using celerity::access::one_to_one;
-
-	TEST_CASE("deprecated range mapper templates continue to work", "[range-mapper][deprecated]") {
-		const auto chunk1d = chunk<1>{1, 2, 3};
-		const auto chunk2d = chunk<2>{{1, 1}, {2, 2}, {3, 3}};
-		const auto chunk3d = chunk<3>{{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
-		const auto range1d = range<1>{4};
-		const auto range2d = range<2>{4, 4};
-		const auto range3d = range<3>{4, 4, 4};
-		const auto subrange1d = subrange<1>{{}, range1d};
-		const auto subrange2d = subrange<2>{{}, range2d};
-		const auto subrange3d = subrange<3>{{}, range3d};
-
-		CHECK(one_to_one<1>{}(chunk1d) == subrange{chunk1d});
-		CHECK(one_to_one<2>{}(chunk2d) == subrange{chunk2d});
-		CHECK(one_to_one<3>{}(chunk3d) == subrange{chunk3d});
-
-		CHECK(fixed<1, 3>{subrange3d}(chunk1d) == subrange3d);
-		CHECK(fixed<2, 2>{subrange2d}(chunk2d) == subrange2d);
-		CHECK(fixed<3, 1>{subrange1d}(chunk3d) == subrange1d);
-
-		CHECK(all<1>{}(chunk1d, range1d) == subrange1d);
-		CHECK(all<2>{}(chunk2d, range2d) == subrange2d);
-		CHECK(all<3>{}(chunk3d, range3d) == subrange3d);
-		CHECK(all<1, 3>{}(chunk1d, range3d) == subrange3d);
-		CHECK(all<2, 2>{}(chunk2d, range2d) == subrange2d);
-		CHECK(all<3, 1>{}(chunk3d, range1d) == subrange1d);
-	}
-
 	TEST_CASE_METHOD(test_utils::runtime_fixture,
 	    "distr_queue::submit(allow_by_ref_t, ...) and creation of accessors/side-effects/reductions from const buffers/host-objects continues to work",
 	    "[handler][deprecated]") {
