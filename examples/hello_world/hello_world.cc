2e77c6b0198ac5b8d8736d86810f43df80b79bdb
Peter Thoman <petert@dps.uibk.ac.at>
2023-07-13 11:22:45 +0200

    Add "Hello World" example

---
+++ examples/hello_world/hello_world.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+#include <celerity.h>
+
+int main() {
+	const std::string input_str = "Ifmmp!Xpsme\"\x01";
+
+	celerity::distr_queue queue;
+	celerity::buffer<char> str_buffer(input_str.data(), input_str.size());
+
+	queue.submit([&](celerity::handler& cgh) {
+		celerity::accessor str_acc{str_buffer, cgh, celerity::access::one_to_one{}, celerity::read_write};
+		cgh.parallel_for(str_buffer.get_range(), [=](celerity::item<1> item) { str_acc[item] -= 1; });
+	});
+
+	auto output = celerity::experimental::fence(queue, str_buffer);
+	std::cout << output.get().get_data() << std::endl;
+}
