47bee9c835d45138b0c596451adfe9fd450c371a
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2023-10-25 13:23:34 +0200

    Add test for GDB pretty-printers

---
+++ test/debug/pretty_printables.cc
@@ -0,0 +1,43 @@
+#include "distributed_graph_generator.h"
+#include "grid.h"
+#include "ranges.h"
+#include "region_map.h"
+#include "types.h"
+
+int main() {
+	[[maybe_unused]] celerity::detail::task_id tid = 10;
+	[[maybe_unused]] celerity::detail::buffer_id bid = 11;
+	[[maybe_unused]] celerity::detail::node_id nid = 12;
+	[[maybe_unused]] celerity::detail::command_id cid = 13;
+	[[maybe_unused]] celerity::detail::collective_group_id cgid = 14;
+	[[maybe_unused]] celerity::detail::reduction_id rid = 15;
+	[[maybe_unused]] celerity::detail::host_object_id hoid = 16;
+	[[maybe_unused]] celerity::detail::hydration_id hyid = 17;
+	[[maybe_unused]] celerity::detail::transfer_id trid = 18;
+
+	[[maybe_unused]] celerity::id<3> id(1, 2, 3);
+	[[maybe_unused]] celerity::range<3> range(1, 2, 3);
+	[[maybe_unused]] celerity::subrange<3> subrange(celerity::id(1, 2, 3), celerity::range(4, 5, 6));
+	[[maybe_unused]] celerity::chunk<3> chunk(celerity::id(1, 2, 3), celerity::range(4, 5, 6), celerity::range(7, 8, 9));
+	[[maybe_unused]] celerity::nd_range<3> nd_range(celerity::range(2, 4, 6), celerity::range(1, 2, 3), celerity::id(7, 8, 9));
+	[[maybe_unused]] celerity::detail::box<3> box(celerity::id(1, 2, 3), celerity::id(4, 5, 6));
+	[[maybe_unused]] celerity::detail::region<3> empty_region;
+	[[maybe_unused]] celerity::detail::region<3> region({
+	    celerity::detail::box(celerity::id(1, 2, 3), celerity::id(4, 5, 6)),
+	    celerity::detail::box(celerity::id(11, 2, 3), celerity::id(14, 5, 6)),
+	    celerity::detail::box(celerity::id(21, 2, 3), celerity::id(24, 5, 6)),
+	});
+
+	[[maybe_unused]] celerity::detail::region_map<int> region_map(celerity::range<3>(10, 10, 10), 3);
+	region_map.update_region(celerity::detail::box<3>({1, 1, 1}, {5, 5, 5}), 42);
+	region_map.update_region(celerity::detail::box<3>({1, 1, 1}, {3, 3, 3}), 69);
+	region_map.update_region(celerity::detail::box<3>({1, 1, 1}, {2, 2, 2}), 1337);
+
+	[[maybe_unused]] celerity::detail::write_command_state wcs_fresh(celerity::detail::command_id(123));
+	[[maybe_unused]] celerity::detail::write_command_state wcs_stale(celerity::detail::command_id(123));
+	wcs_stale.mark_as_stale();
+	[[maybe_unused]] celerity::detail::write_command_state wcs_replicated(celerity::detail::command_id(123), true /* replicated */);
+
+	// tell GDB to break here so we can examine locals
+	__builtin_trap();
+}
