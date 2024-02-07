14f324e6b0008cc6406ed6fedf77185b48b28fdd
Philip Salzmann <philip.salzmann@uibk.ac.at>
2024-02-07 16:23:45 +0100

    CI: Complain if clang-format is not installed

--- ci/find-unformatted-files.sh
+++ ci/find-unformatted-files.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -o pipefail -o nounset
+
 if [[ ! -d src ]]; then
     echo "Warning: This script should be run from within the repository root directory" >&2
+fi
+
+if [[ ! -x "$(which clang-format)" ]]; then
+    echo "Error: clang-format is not installed" >&2
+    exit 1
 fi
 
 SOURCES=$(find examples include src test \( -name "*.h" -o -name "*.cc" \) ! -name "stb*")
@@ -11,6 +18,6 @@ for s in $SOURCES; do
     # we use the XML replacements output together with grep as a workaround.
     NUM_REPLACEMENTS=$(clang-format -output-replacements-xml "$s" | grep -v -c -E "<(/?replacements|\?xml)")
     if [[ $NUM_REPLACEMENTS -ne 0 ]]; then
-        echo $s
+        echo "$s"
     fi
 done
