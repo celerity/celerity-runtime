45ad5330591f549e5f20c0dcfc8802e060793b2d
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2024-03-12 16:56:15 +0100

    Add new communicator / receive_arbiter infrastructure

---
+++ include/receive_arbiter.h
@@ -0,0 +1,149 @@
+#pragma once
+
+#include "communicator.h"
+#include "pilot.h"
+
+#include <unordered_map>
+#include <variant>
+
+namespace celerity::detail::receive_arbiter_detail {
+
+/// A single box received by the communicator, as described earlier by an inbound pilot.
+struct incoming_region_fragment {
+	detail::box<3> box;
+	async_event communication; ///< async communicator event for receiving this fragment
+};
+
+/// State for a single incomplete `receive` operation or a `begin_split_receive` / `await_split_receive_subregion` tree.
+struct region_request {
+	void* allocation;
+	box<3> allocated_box;
+	region<3> incomplete_region;
+	std::vector<incoming_region_fragment> incoming_fragments;
+
+	region_request(region<3> requested_region, void* const allocation, const box<3>& allocated_bounding_box)
+	    : allocation(allocation), allocated_box(allocated_bounding_box), incomplete_region(std::move(requested_region)) {}
+	bool do_complete();
+};
+
+/// A single chunk in a `gather_request` that is currently being received by the communicator.
+struct incoming_gather_chunk {
+	async_event communication; ///< async communicator event for receiving this chunk
+};
+
+/// State for a single incomplete `gather_receive` operation.
+struct gather_request {
+	void* allocation;
+	size_t chunk_size;                                  ///< in bytes
+	size_t num_incomplete_chunks;                       ///< number of chunks that are currently being received or for which we have not seen a pilot yet
+	std::vector<incoming_gather_chunk> incoming_chunks; ///< chunks that are currently being received
+
+	gather_request(void* const allocation, const size_t chunk_size, const size_t num_total_chunks)
+	    : allocation(allocation), chunk_size(chunk_size), num_incomplete_chunks(num_total_chunks) {}
+	bool do_complete();
+};
+
+// shared_ptrs for pointer stability (referenced by receive_arbiter::event)
+using stable_region_request = std::shared_ptr<region_request>;
+using stable_gather_request = std::shared_ptr<gather_request>;
+
+/// A transfer that is only known through inbound pilots so far, but no `receive` / `begin_split_receive` has been issued so far.
+struct unassigned_transfer {
+	std::vector<inbound_pilot> pilots;
+	bool do_complete();
+};
+
+/// A (non-gather) transfer that has been mentioned in one or more calls to `receive` / `begin_split_receive`. Note that there may be multiple disjoint
+/// receives mapping to the same `transfer_id` as long as their regions are pairwise disconnected.
+struct multi_region_transfer {
+	size_t elem_size;                                   ///< in bytes
+	std::vector<stable_region_request> active_requests; ///< all `receive`s and `begin_split_receive`s active for this transfer id.
+	std::vector<inbound_pilot> unassigned_pilots;       ///< all inbound pilots that do not map to any `active_request`.
+
+	explicit multi_region_transfer(const size_t elem_size) : elem_size(elem_size) {}
+	explicit multi_region_transfer(const size_t elem_size, std::vector<inbound_pilot>&& unassigned_pilots)
+	    : elem_size(elem_size), unassigned_pilots(std::move(unassigned_pilots)) {}
+	bool do_complete();
+};
+
+/// A transfer originating through `gather_receive`. It is fully described by a single `gather_request`.
+struct gather_transfer {
+	stable_gather_request request;
+	bool do_complete();
+};
+
+/// Depending on the order of inputs, transfers may start out as unassigned and will be replaced by either `multi_region_transfer`s or `gather_transfer`s
+/// once explicit calls to the respective receive arbiter functions are made.
+using transfer = std::variant<unassigned_transfer, multi_region_transfer, gather_transfer>;
+
+} // namespace celerity::detail::receive_arbiter_detail
+
+namespace celerity::detail {
+
+/// Matches receive instructions to inbound pilots and trigger in-place payload receives on the communicator.
+///
+/// For scalability reasons, distributed command graph generation only yields exact destinations and buffer sub-ranges for push commands, while await-pushes do
+/// not carry such information - they just denote the full region to be received. Sender nodes later communicate the exact ranges to the receiver during
+/// execution time via pilot messages that are generated alongside the instruction graph.
+///
+/// The receive_arbiter's job is to match these inbound pilots to receive instructions generated from await-push commands to issue in-place receives (i.e.
+/// `MPI_Recv`) of the data into an appropriate host allocation. Since these inputs may arrive in arbitrary order, it maintains a separate state machine for
+/// each `transfer_id` to drive all operations that eventually result in completing an `async_event` for each receive instruction.
+class receive_arbiter {
+  public:
+	/// `receive_arbiter` will use `comm` to poll for inbound pilots and issue payload-receives.
+	explicit receive_arbiter(communicator& comm);
+
+	receive_arbiter(const receive_arbiter&) = delete;
+	receive_arbiter(receive_arbiter&&) = default;
+	receive_arbiter& operator=(const receive_arbiter&) = delete;
+	receive_arbiter& operator=(receive_arbiter&&) = default;
+	~receive_arbiter();
+
+	/// Receive a buffer region associated with a single transfer id `trid` into an existing `allocation` with size `allocated_box.size() * elem_size`. The
+	/// `request` region must be fully contained in `allocated_box`, and the caller must ensure that it the communicator will not receive an inbound pilot that
+	/// intersects `request` without being fully contained in it. The returned `async_event` will complete once the receive is complete.
+	[[nodiscard]] async_event receive(const transfer_id& trid, const region<3>& request, void* allocation, const box<3>& allocated_box, size_t elem_size);
+
+	/// Begin the reception of a buffer region into an existing allocation similar to `receive`, but do not await its completion with a single `async_event`.
+	/// Instead, the caller must follow up with calls to `await_split_receive_subregion` to the same `transfer_id` whose request regions do not necessarily have
+	/// to be disjoint, but whose union must be equal to the original `request`.
+	void begin_split_receive(const transfer_id& trid, const region<3>& request, void* allocation, const box<3>& allocated_box, size_t elem_size);
+
+	/// To be called after `begin_split_receive` to await receiving a `subregion` of the original request. Subregions passed to different invocations of this
+	/// function may overlap, but must not exceed the original request. If the entire split-receive has finished already, this will return an instantly complete
+	/// event.
+	[[nodiscard]] async_event await_split_receive_subregion(const transfer_id& trid, const region<3>& subregion);
+
+	/// Receive a contiguous chunk of data from every peer node, placing the results in `allocation[node_chunk_size * node_id]`. The location reserved for the
+	/// local node is not written to and may be concurrently accessed while this operation is in progress. If a peer node announces that it will not contribute
+	/// to this transfer by sending an empty-box pilot, its location will also remain unmodified.
+	///
+	/// This feature is a temporary solution until we implement inter-node reductions through inter-node collectives.
+	[[nodiscard]] async_event gather_receive(const transfer_id& trid, void* allocation, size_t node_chunk_size);
+
+	/// Polls the communicator for inbound pilots and advances the state of all ongoing receive operations. This is expected to be called in a loop
+	/// unconditionally.
+	void poll_communicator();
+
+  private:
+	communicator* m_comm;
+	size_t m_num_nodes;
+
+	/// State machines for all `transfer_id`s that were mentioned in an inbound pilot or call to one of the receive functions. Once a transfer is complete, it
+	/// is cleared from `m_transfers`, but `multi_region_transfer`s can be re-created if there later appears another pair of inbound pilots and `receive`s for
+	/// the same transfer id that did not temporally overlap with the original ones.
+	std::unordered_map<transfer_id, receive_arbiter_detail::transfer> m_transfers;
+
+	/// Initiates a new `region_request` for which the caller can construct events to await either the entire region or sub-regions.
+	receive_arbiter_detail::stable_region_request& initiate_region_request(
+	    const transfer_id& trid, const region<3>& request, void* allocation, const box<3>& allocated_box, size_t elem_size);
+
+	/// Updates the state of an active `region_request` from receiving an inbound pilot.
+	void handle_region_request_pilot(receive_arbiter_detail::region_request& rr, const inbound_pilot& pilot, size_t elem_size);
+
+	/// Updates the state of an active `gather_request` from receiving an inbound pilot.
+	void handle_gather_request_pilot(receive_arbiter_detail::gather_request& gr, const inbound_pilot& pilot);
+};
+
+} // namespace celerity::detail
---
+++ test/system/mpi_tests.cc
@@ -0,0 +1,327 @@
+#include "../test_utils.h"
+
+#include "communicator.h"
+#include "mpi_communicator.h"
+#include "types.h"
+
+#include <thread>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_range.hpp>
+
+
+using namespace celerity;
+using namespace celerity::detail;
+using namespace std::chrono_literals;
+
+
+namespace celerity::detail {
+
+struct mpi_communicator_testspy {
+	static size_t get_num_cached_array_types(const mpi_communicator& comm) { return comm.m_array_type_cache.size(); }
+	static size_t get_num_cached_scalar_types(const mpi_communicator& comm) { return comm.m_scalar_type_cache.size(); }
+};
+
+} // namespace celerity::detail
+
+
+TEST_CASE_METHOD(test_utils::mpi_fixture, "mpi_communicator sends and receives pilot messages", "[mpi]") {
+	mpi_communicator comm(collective_clone_from, MPI_COMM_WORLD);
+	const auto num_nodes = comm.get_num_nodes();
+	const auto self = comm.get_local_node_id();
+	CAPTURE(num_nodes, self);
+
+	if(num_nodes <= 1) { SKIP("test must be run on at least 2 ranks"); }
+
+	const auto make_pilot_message = [&](const node_id sender, const node_id receiver) {
+		// Compute a unique id for the (sender, receiver) tuple and base all other members of the pilot message on this ID to test that we receive the correct
+		// pilots on the correct nodes (and everything remains uncorrupted).
+		const auto p2p_id = 1 + sender * num_nodes + receiver;
+		const message_id msgid = p2p_id * 13;
+		const buffer_id bid = p2p_id * 11;
+		const task_id consumer_tid = p2p_id * 17;
+		const reduction_id rid = p2p_id * 19;
+		const transfer_id trid(consumer_tid, bid, rid);
+		const box<3> box = {id{p2p_id, p2p_id * 2, p2p_id * 3}, id{p2p_id * 4, p2p_id * 5, p2p_id * 6}};
+		return outbound_pilot{receiver, pilot_message{msgid, trid, box}};
+	};
+
+	// Send a pilot from each node to each other node
+	for(node_id other = 0; other < num_nodes; ++other) {
+		if(other == self) continue;
+		comm.send_outbound_pilot(make_pilot_message(self, other));
+	}
+
+	size_t num_pilots_received = 0;
+	while(num_pilots_received < num_nodes - 1) {
+		// busy-wait for all expected pilots to arrive
+		for(const auto& pilot : comm.poll_inbound_pilots()) {
+			CAPTURE(pilot.from);
+			const auto expect = make_pilot_message(pilot.from, self);
+			CHECK(pilot.message.id == expect.message.id);
+			CHECK(pilot.message.transfer_id == expect.message.transfer_id);
+			CHECK(pilot.message.box == expect.message.box);
+			++num_pilots_received;
+		}
+	}
+
+	SUCCEED("it didn't deadlock 👏");
+}
+
+TEST_CASE_METHOD(test_utils::mpi_fixture, "mpi_communicator sends and receives payloads", "[mpi]") {
+	mpi_communicator comm(collective_clone_from, MPI_COMM_WORLD);
+	const auto num_nodes = comm.get_num_nodes();
+	const auto self = comm.get_local_node_id();
+	CAPTURE(num_nodes, self);
+
+	if(num_nodes <= 1) { SKIP("test must be run on at least 2 ranks"); }
+
+	const auto make_msgid = [=](const node_id sender, const node_id receiver) { //
+		return message_id(1 + sender * num_nodes + receiver);
+	};
+
+	constexpr static communicator::stride stride{{12, 11, 11}, {{1, 0, 3}, {5, 4, 6}}, sizeof(int)};
+
+	std::vector<std::vector<int>> send_buffers;
+	std::vector<std::vector<int>> receive_buffers;
+	std::vector<async_event> events;
+	for(node_id other = 0; other < num_nodes; ++other) {
+		if(other == self) continue;
+
+		// allocate a send buffer and fill with a (sender, receiver) specific pattern that can be tested after receiving
+		auto& send = send_buffers.emplace_back(stride.allocation_range.size());
+		std::iota(send.begin(), send.end(), make_msgid(self, other));
+
+		// allocate and zero-fill a receive buffer (zero is never part of a payload)
+		auto& receive = receive_buffers.emplace_back(stride.allocation_range.size());
+
+		// start send and receive operations
+		events.push_back(comm.send_payload(other, make_msgid(self, other), send.data(), stride));
+		events.push_back(comm.receive_payload(other, make_msgid(other, self), receive.data(), stride));
+	}
+
+	// busy-wait for all send / receive events to complete
+	while(!events.empty()) {
+		const auto end_incomplete = std::remove_if(events.begin(), events.end(), std::mem_fn(&async_event::is_complete));
+		events.erase(end_incomplete, events.end());
+	}
+
+	auto received = receive_buffers.begin();
+	for(node_id other = 0; other < num_nodes; ++other) {
+		if(other == self) continue;
+
+		// reconstruct the expected receive buffer
+		std::vector<int> other_send(stride.allocation_range.size());
+		std::iota(other_send.begin(), other_send.end(), make_msgid(other, self));
+		std::vector<int> expected(stride.allocation_range.size());
+		test_utils::for_each_in_range(stride.transfer.range, stride.transfer.offset, [&](const id<3>& id) {
+			const auto linear_index = get_linear_index(stride.allocation_range, id);
+			expected[linear_index] = other_send[linear_index];
+		});
+
+		CHECK(*received == expected);
+		++received; // not equivalent to receive_buffers[other] because we skip `other == self`
+	}
+}
+
+// We require that it's well-defined to send a scalar from a n-dimensional stride and receive it in an m-dimensional stride, since stride dimensionality is
+// determined from effective allocation dimensionality, which can vary between participating nodes depending on the size of their buffer host allocations.
+TEST_CASE_METHOD(test_utils::mpi_fixture, "mpi_communicator correctly transfers scalars between strides of different dimensionality", "[mpi]") {
+	// All GENERATEs must happen before an early-return, otherwise different nodes will execute this test case different numbers of times
+	const auto send_dims = GENERATE(values<size_t>({0, 1, 2, 3}));
+	const auto recv_dims = GENERATE(values<size_t>({0, 1, 2, 3}));
+	CAPTURE(send_dims, recv_dims);
+
+	mpi_communicator comm(collective_clone_from, MPI_COMM_WORLD);
+	const auto num_nodes = comm.get_num_nodes();
+	const auto local_node_id = comm.get_local_node_id();
+	CAPTURE(num_nodes, local_node_id);
+
+	if(num_nodes <= 1) { SKIP("test must be run on at least 2 ranks"); }
+	if(local_node_id >= 2) return; // needs exactly 2 nodes to participate
+
+	constexpr communicator::stride dim_strides[] = {
+	    {{1, 1, 1}, {{0, 0, 0}, {1, 1, 1}}, 4}, // 0-dimensional
+	    {{2, 1, 1}, {{1, 0, 0}, {1, 1, 1}}, 4}, // 1-dimensional
+	    {{2, 3, 1}, {{1, 2, 0}, {1, 1, 1}}, 4}, // 2-dimensional
+	    {{2, 3, 5}, {{1, 2, 3}, {1, 1, 1}}, 4}, // 3-dimensional
+	};
+
+	const auto& send_stride = dim_strides[send_dims];
+	const auto& recv_stride = dim_strides[recv_dims];
+
+	std::vector<int> buf(dim_strides[3].allocation_range.size());
+	async_event evt;
+	if(local_node_id == 1) { // sender
+		buf[get_linear_index(send_stride.allocation_range, send_stride.transfer.offset)] = 42;
+		evt = comm.send_payload(0, 99, buf.data(), send_stride);
+	} else { // receiver
+		evt = comm.receive_payload(1, 99, buf.data(), recv_stride);
+	}
+	// busy-wait for event
+	while(!evt.is_complete()) {}
+
+	if(local_node_id == 0) { // receiver
+		std::vector<int> expected(dim_strides[3].allocation_range.size());
+		expected[get_linear_index(recv_stride.allocation_range, recv_stride.transfer.offset)] = 42;
+		CHECK(buf == expected);
+	}
+}
+
+TEST_CASE_METHOD(test_utils::mpi_fixture, "mpi_communicator correctly transfers boxes that map to different subranges on sender and receiver", "[mpi]") {
+	// All GENERATEs must happen before an early-return, otherwise different nodes will execute this test case a different number of times
+	const auto dims = GENERATE(values<int>({1, 2, 3}));
+	CAPTURE(dims);
+
+	mpi_communicator comm(collective_clone_from, MPI_COMM_WORLD);
+	const auto num_nodes = comm.get_num_nodes();
+	const auto local_node_id = comm.get_local_node_id();
+	CAPTURE(num_nodes, local_node_id);
+
+	if(num_nodes <= 1) { SKIP("test must be run on at least 2 ranks"); }
+	if(local_node_id >= 2) return; // needs exactly 2 nodes
+
+	range box_range{3, 4, 5};
+	range sender_allocation{10, 7, 11};
+	id sender_offset{1, 2, 3};
+	range receiver_allocation{8, 10, 13};
+	id receiver_offset{2, 0, 4};
+	// manually truncate to runtime value `dims`
+	for(int d = dims; d < 3; ++d) {
+		box_range[d] = 1;
+		sender_allocation[d] = 1;
+		sender_offset[d] = 0;
+		receiver_allocation[d] = 1;
+		receiver_offset[d] = 0;
+	}
+
+	std::vector<int> send_buf(sender_allocation.size());
+	std::vector<int> recv_buf(receiver_allocation.size());
+
+	std::iota(send_buf.begin(), send_buf.end(), 0);
+
+	async_event evt;
+	if(local_node_id == 1) { // sender
+		evt = comm.send_payload(0, 99, send_buf.data(), communicator::stride{sender_allocation, subrange{sender_offset, box_range}, sizeof(int)});
+	} else { // receiver
+		evt = comm.receive_payload(1, 99, recv_buf.data(), communicator::stride{receiver_allocation, subrange{receiver_offset, box_range}, sizeof(int)});
+	}
+	while(!evt.is_complete()) {} // busy-wait for evt
+
+	if(local_node_id == 0) {
+		std::vector<int> expected(receiver_allocation.size());
+		test_utils::for_each_in_range(box_range, [&](const id<3>& id) {
+			const auto sender_idx = get_linear_index(sender_allocation, sender_offset + id);
+			const auto receiver_idx = get_linear_index(receiver_allocation, receiver_offset + id);
+			expected[receiver_idx] = send_buf[sender_idx];
+		});
+		CHECK(recv_buf == expected);
+	}
+}
+
+TEST_CASE_METHOD(test_utils::mpi_fixture, "collectives are concurrent between distinct mpi_communicators", "[mpi]") {
+	constexpr static size_t concurrency = 16;
+
+	// create a bunch of communicators that we can then operate on from concurrent threads
+	std::vector<std::unique_ptr<communicator>> roots;
+	for(size_t i = 0; i < concurrency; ++i) {
+		roots.push_back(std::make_unique<mpi_communicator>(collective_clone_from, MPI_COMM_WORLD));
+	}
+
+	// for each communicator, spawn a thread that creates more communicators
+	std::vector<std::vector<std::unique_ptr<communicator>>> concurrent_clones(concurrency);
+	std::vector<std::thread> concurrent_threads(concurrency);
+	for(size_t i = 0; i < concurrency; ++i) {
+		concurrent_threads[i] = std::thread([&, i] {
+			for(size_t j = 0; j < concurrency; ++j) {
+				concurrent_clones[i].push_back(roots[i]->collective_clone());
+				std::this_thread::sleep_for(10ms); // ensure the OS doesn't serialize all threads by chance
+			}
+		});
+	}
+	for(size_t i = 0; i < concurrency; ++i) {
+		concurrent_threads[i].join();
+	}
+
+	// flip the iteration order and issue a barrier from each new collective group
+	for(size_t i = 0; i < concurrency; ++i) {
+		concurrent_threads[i] = std::thread([&, i] {
+			for(size_t j = 0; j < concurrency; ++j) {
+				concurrent_clones[j][i]->collective_barrier();
+				std::this_thread::sleep_for(10ms); // ensure the OS doesn't serialize all threads by chance
+			}
+		});
+	}
+	for(size_t i = 0; i < concurrency; ++i) {
+		concurrent_threads[i].join();
+	}
+
+	// ~mpi_communicator is also a collective operation; and it shouldn't matter if we destroy parents before their children
+	roots.clear();
+
+	for(size_t i = 0; i < concurrency; ++i) {
+		concurrent_threads[i] = std::thread([&, i] {
+			concurrent_clones[i].clear(); // ~mpi_communicator is a collective operation
+		});
+	}
+	for(size_t i = 0; i < concurrency; ++i) {
+		concurrent_threads[i].join();
+	}
+
+	SUCCEED("it didn't deadlock or crash 🎉");
+}
+
+TEST_CASE("mpi_communicator normalizes strides to cache and re-uses MPI data types", "[mpi]") {
+	static const std::vector<std::vector<communicator::stride>> sets_of_equivalent_strides{
+	    // strides only differ in allocation size / offset in dim 0 and can be normalized by adjusting the base pointer
+	    {
+	        {{13, 12, 11}, {{1, 0, 3}, {5, 4, 6}}, sizeof(int)},
+	        {{5, 12, 11}, {{0, 0, 3}, {5, 4, 6}}, sizeof(int)},
+	        {{20, 12, 11}, {{4, 0, 3}, {5, 4, 6}}, sizeof(int)},
+	    },
+	    {
+	        {{13, 1, 1}, {{1, 0, 0}, {5, 1, 1}}, sizeof(int)},
+	        {{1, 13, 1}, {{0, 1, 0}, {1, 5, 1}}, sizeof(int)},
+	        {{1, 1, 13}, {{0, 0, 1}, {1, 1, 5}}, sizeof(int)},
+	    },
+	};
+	// All GENERATEs must happen before an early-return, otherwise different nodes will execute this test case different numbers of times
+	const auto equivalent_strides = GENERATE(from_range(sets_of_equivalent_strides));
+
+	mpi_communicator comm(collective_clone_from, MPI_COMM_WORLD);
+	const auto num_nodes = comm.get_num_nodes();
+	const auto self = comm.get_local_node_id();
+	CAPTURE(num_nodes, self);
+
+	if(num_nodes <= 1) { SKIP("test must be run on at least 2 ranks"); }
+	if(self >= 2) return; // needs exactly 2 nodes
+	const node_id peer = 1 - self;
+
+	message_id msgid = 0;
+	for(int repeat = 0; repeat < 2; ++repeat) {
+		CAPTURE(repeat);
+		for(const auto& stride : equivalent_strides) {
+			CAPTURE(stride.allocation_range, stride.transfer, stride.element_size);
+			std::vector<int> send_buf(stride.allocation_range.size());
+			std::iota(send_buf.begin(), send_buf.end(), 1);
+			const auto send_evt = comm.send_payload(peer, msgid, std::as_const(send_buf).data(), stride);
+
+			std::vector<int> recv_buf(stride.allocation_range.size());
+			const auto recv_event = comm.receive_payload(peer, msgid, recv_buf.data(), stride);
+
+			while(!send_evt.is_complete() || !recv_event.is_complete()) {} // busy-wait for events to complete
+
+			std::vector<int> expected(stride.allocation_range.size());
+			test_utils::for_each_in_range(stride.transfer.range, stride.transfer.offset, [&](const id<3>& id) {
+				const auto linear_id = get_linear_index(stride.allocation_range, id);
+				expected[linear_id] = send_buf[linear_id];
+			});
+
+			CHECK(recv_buf == expected);
+			++msgid;
+		}
+	}
+
+	CHECK(mpi_communicator_testspy::get_num_cached_array_types(comm) == 1);  // all strides we sent/received were equivalent under normalization
+	CHECK(mpi_communicator_testspy::get_num_cached_scalar_types(comm) == 1); // only scalar type used was int
+}
