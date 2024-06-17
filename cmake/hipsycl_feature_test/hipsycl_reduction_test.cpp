648da9288dce02254359344481d65df25c2dc24a
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2024-06-17 09:44:14 +0200

    Unconditionally disable reductions on hipSYCL

--- cmake/hipsycl_feature_test/hipsycl_reduction_test.cpp
+++
@@ -1,8 +0,0 @@
-#include <CL/sycl.hpp>
-
-int main() {
-	cl::sycl::buffer<int> b{{1}};
-	cl::sycl::queue{}.submit([&](cl::sycl::handler& cgh) {
-		cgh.parallel_for(cl::sycl::range<1>{1}, cl::sycl::reduction(b, cgh, cl::sycl::plus<int>{}), [](cl::sycl::item<1>, auto& r) { r += 1; });
-	});
-}
