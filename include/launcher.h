b3d3702e4de4dabb94bc9378613c7f66bc95fa16
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2024-06-29 19:11:21 +0200

    Move host_queue out of host_task_launcher signature
    Launchers were not invoked in existing code so far, but host_queue will
    be removed with IDAG execution.

--- include/launcher.h
+++ include/launcher.h
@@ -1,19 +1,20 @@
 #pragma once
 
-#include "async_event.h"
 #include "grid.h"
-#include "host_queue.h"
 
 #include <functional>
 #include <variant>
 #include <vector>
 
-#include <mpi.h>
+#include <sycl/sycl.hpp>
 
 namespace celerity::detail {
 
+class communicator;
+class host_queue;
+
 using device_kernel_launcher = std::function<void(sycl::handler& sycl_cgh, const box<3>& execution_range, const std::vector<void*>& reduction_ptrs)>;
-using host_task_launcher = std::function<async_event(host_queue& q, const box<3>& execution_range, MPI_Comm mpi_comm)>;
+using host_task_launcher = std::function<void(const box<3>& execution_range, const communicator* collective_comm)>;
 using command_group_launcher = std::variant<device_kernel_launcher, host_task_launcher>;
 
 } // namespace celerity::detail
