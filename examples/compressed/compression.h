1dcda24ef9782afe645da40e43cf7f153f74c54c
Gabriel Mitterrutzner <Gabriel.Mitterrutzner@student.uibk.ac.at>
2024-08-12 14:09:49 +0200

    update quantization (now including sycl::vec as possible type)

--- examples/compressed/compression.h
+++ examples/compressed/compression.h
@@ -9,52 +9,120 @@ namespace celerity {
 template <typename T>
 class compressed {};
 
+// Primary template for is_vec
+template <typename T>
+struct is_vec : std::false_type {};
+
+// Specialization for sycl::vec
+template <typename T, int N>
+struct is_vec<sycl::vec<T, N>> : std::true_type {};
+
+template <typename T>
+struct vec_element_type {
+	using type = T;
+};
+
+template <typename T, int N>
+struct vec_element_type<sycl::vec<T, N>> {
+	using type = T;
+};
+
+template <typename T>
+struct vec_size {
+	static constexpr int value = 1;
+};
+
+template <typename T, int N>
+struct vec_size<sycl::vec<T, N>> {
+	static constexpr int value = N;
+};
+
 template <typename T, typename Q>
 class compressed<celerity::compression::quantization<T, Q>> {
 	using quant_type = typename celerity::compression::quantization<T, Q>::quant_type;
 	using value_type = typename celerity::compression::quantization<T, Q>::value_type;
 
+	using vec_value_type = typename vec_element_type<value_type>::type;
+	using vec_quant_type = typename vec_element_type<quant_type>::type;
+
   public:
-	compressed() : m_upper_bound(0), m_lower_bound(0) {}
+	compressed() : m_lower_bound(0), m_upper_bound(1) {}
 
 	template <typename ValueT>
-	compressed(ValueT upper_bound, ValueT lower_bound) : m_upper_bound(upper_bound), m_lower_bound(lower_bound) {
-		static_assert(std::is_same<ValueT, value_type>(), "Value type isn't the same");
+	compressed(ValueT lower_bound, ValueT upper_bound) : m_lower_bound(lower_bound), m_upper_bound(upper_bound) {
+		static_assert(std::is_same<ValueT, vec_value_type>(), "Value type isn't the same");
+		static_assert(is_vec<value_type>::value == is_vec<quant_type>::value, "Value and Quant type must be both either sycl::vec or fundamental");
 	}
 
-	value_type get_upper_bound() const { return m_upper_bound; }
-	value_type get_lower_bound() const { return m_lower_bound; }
+	vec_value_type get_upper_bound() const { return m_upper_bound; }
+	vec_value_type get_lower_bound() const { return m_lower_bound; }
 
-	void set_upper_bound(value_type upper_bound) { m_upper_bound = upper_bound; }
-	void set_lower_bound(value_type lower_bound) { m_lower_bound = lower_bound; }
+	void set_upper_bound(vec_value_type upper_bound) { m_upper_bound = upper_bound; }
+	void set_lower_bound(vec_value_type lower_bound) { m_lower_bound = lower_bound; }
 
 	quant_type compress(const value_type number) const {
-		return static_cast<quant_type>(std::round((number - m_lower_bound) / (m_upper_bound - m_lower_bound) * std::numeric_limits<quant_type>::max()));
+		if constexpr(is_vec<value_type>::value) {
+			quant_type result;
+			for(int i = 0; i < vec_size<quant_type>::value; ++i) {
+				result[i] = static_cast<vec_quant_type>(
+				    std::round((number[i] - m_lower_bound) / (m_upper_bound - m_lower_bound) * std::numeric_limits<vec_quant_type>::max()));
+			}
+
+			return result;
+		} else {
+			return static_cast<quant_type>(std::round((number - m_lower_bound) / (m_upper_bound - m_lower_bound) * std::numeric_limits<quant_type>::max()));
+		}
 	}
 
 	value_type decompress(const quant_type number) const {
-		return static_cast<value_type>(number) / static_cast<value_type>(std::numeric_limits<quant_type>::max()) * (m_upper_bound - m_lower_bound)
-		       + m_lower_bound;
+		if constexpr(is_vec<value_type>::value) {
+			value_type result;
+
+			for(int i = 0; i < vec_size<value_type>::value; ++i) {
+				result[i] = static_cast<vec_value_type>(number[i]) / static_cast<vec_value_type>(std::numeric_limits<vec_quant_type>::max())
+				                * (m_upper_bound - m_lower_bound)
+				            + m_lower_bound;
+			}
+
+			return result;
+		} else {
+			return static_cast<value_type>(number) / static_cast<value_type>(std::numeric_limits<quant_type>::max()) * (m_upper_bound - m_lower_bound)
+			       + m_lower_bound;
+		}
 	}
 
-	// compress value_type* to keep_alive
-	const std::vector<quant_type> compress_data(const value_type* data, const size_t size) {
+	std::vector<quant_type> compress_data(const value_type* data, const size_t size) {
 		std::vector<quant_type> keep_alive(size);
 
-		// find upper and lower bound
-		if(m_upper_bound == m_lower_bound) {
-			m_upper_bound = *std::max_element(data, data + size);
-			m_lower_bound = *std::min_element(data, data + size);
+		if constexpr(is_vec<value_type>::value) {
+			if(m_upper_bound == m_lower_bound) {
+				vec_value_type max = m_upper_bound;
+				vec_value_type min = m_lower_bound;
+
+				for(size_t i = 0; i < size; ++i) {
+					for(int j = 0; j < vec_size<value_type>::value; ++j) {
+						max = std::max(max, data[i][j]);
+						min = std::min(min, data[i][j]);
+					}
+				}
+
+				m_upper_bound = max;
+				m_lower_bound = min;
+			}
+		} else {
+			if(m_upper_bound == m_lower_bound) {
+				m_upper_bound = *std::max_element(data, data + size);
+				m_lower_bound = *std::min_element(data, data + size);
+			}
 		}
 
 		std::transform(data, data + size, keep_alive.begin(), [&](const value_type& number) { return compress(number); });
-
 		return std::move(keep_alive);
 	}
 
   private:
-	value_type m_upper_bound;
-	value_type m_lower_bound;
+	vec_value_type m_lower_bound;
+	vec_value_type m_upper_bound;
 };
 
 template <typename T, typename Q>
@@ -98,9 +166,11 @@ class buffer<Intype, Dims, compressed<celerity::compression::quantization<Intype
 	using compression = celerity::compression::quantization<Intype, DataT>;
 
 	buffer(const Intype* data, range<Dims> range, compressed<compression>& compression)
-	    : base(compression.compress_data(data, range.size()).data(), range), m_compression(compression) {}
+	    : buffer(std::move(compression.compress_data(data, range.size())), range, compression) {}
 
-	buffer(range<Dims> range, compressed<compression>& compression) : base(range), m_compression(compression) {}
+	buffer(range<Dims> range, compressed<compression>& compression) : base(range), m_compression(compression) {
+		assert(m_compression.get_lower_bound() != m_compression.get_upper_bound() && "Lower bound is equal to upper bound");
+	}
 
 	// move constructor
 	buffer(buffer&& other) noexcept : base(std::move(other)), m_compression(other.m_compression) {}
@@ -125,7 +195,12 @@ class buffer<Intype, Dims, compressed<celerity::compression::quantization<Intype
 	const compressed<compression>& get_compression() const { return m_compression; }
 
   private:
-	compressed<celerity::compression::quantization<Intype, DataT>>& m_compression;
+	buffer(std::vector<DataT>&& data, range<Dims> range, compressed<compression>& compression)
+	    : base(data.data(), range), m_data(std::move(data)), m_compression(compression) {}
+
+	std::vector<DataT> m_data;
+
+	compressed<celerity::compression::quantization<Intype, DataT>> m_compression;
 };
 
 
@@ -160,6 +235,11 @@ class accessor<DataT, Dims, Mode, Target, compressed<celerity::compression::quan
 		return {base::operator[](index), m_compression};
 	}
 
+	template <int D = Dims, std::enable_if_t<(D > 0), int> = 0>
+	inline decltype(auto) operator[](const size_t dim0) const {
+		return detail::subscript<Dims>(*this, dim0);
+	}
+
 	template <target T = Target, std::enable_if_t<T == target::host_task, int> = 0>
 	inline const std::vector<value_type> get_pointer(range<Dims> new_range) const {
 		auto* new_buff = base::get_pointer();
@@ -168,7 +248,7 @@ class accessor<DataT, Dims, Mode, Target, compressed<celerity::compression::quan
 		std::transform(
 		    new_buff, new_buff + new_range.size(), uncompressed_data.begin(), [&](const quant_type& number) { return m_compression.decompress(number); });
 
-		return uncompressed_data;
+		return std::move(uncompressed_data);
 	}
 
   private:
