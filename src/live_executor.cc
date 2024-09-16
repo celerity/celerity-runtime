49e048f95188903507baa80e8a5414ac9e1ae6a9
Fabian Knorr <fabian.knorr@dps.uibk.ac.at>
2024-09-16 15:52:53 +0200

    Fix log strings in live_executor
    Earlier refactoring lead to a mismatch between format strings and format
    arguments.

--- src/live_executor.cc
+++ src/live_executor.cc
@@ -705,8 +705,7 @@ void executor_impl::issue_async(const alloc_instruction& ainstr, const out_of_or
 	assert(!assignment.lane.has_value());
 	assert(assignment.device.has_value() == (ainstr.get_allocation_id().get_memory_id() > host_memory_id));
 
-	CELERITY_DETAIL_TRACE_INSTRUCTION(
-	    ainstr, "alloc {}, {} % {} bytes", ainstr.get_id(), ainstr.get_allocation_id(), ainstr.get_size_bytes(), ainstr.get_alignment_bytes());
+	CELERITY_DETAIL_TRACE_INSTRUCTION(ainstr, "alloc {}, {} % {} bytes", ainstr.get_allocation_id(), ainstr.get_size_bytes(), ainstr.get_alignment_bytes());
 
 	if(assignment.device.has_value()) {
 		async.event = backend->enqueue_device_alloc(*assignment.device, ainstr.get_size_bytes(), ainstr.get_alignment_bytes());
@@ -722,7 +721,7 @@ void executor_impl::issue_async(const free_instruction& finstr, const out_of_ord
 	const auto ptr = it->second;
 	allocations.erase(it);
 
-	CELERITY_DETAIL_TRACE_INSTRUCTION(finstr, "free {}", finstr.get_id(), finstr.get_allocation_id());
+	CELERITY_DETAIL_TRACE_INSTRUCTION(finstr, "free {}", finstr.get_allocation_id());
 
 	if(assignment.device.has_value()) {
 		async.event = backend->enqueue_device_free(*assignment.device, ptr);
@@ -817,7 +816,7 @@ void executor_impl::issue_async(
 {
 	assert(assignment.target == out_of_order_engine::target::immediate);
 
-	CELERITY_DETAIL_TRACE_INSTRUCTION(sinstr, "send {}+{}, {}x{} bytes to N{} (MSG{})", sinstr.get_id(), sinstr.get_source_allocation_id(),
+	CELERITY_DETAIL_TRACE_INSTRUCTION(sinstr, "send {}+{}, {}x{} bytes to N{} (MSG{})", sinstr.get_source_allocation_id(),
 	    sinstr.get_offset_in_source_allocation(), sinstr.get_send_range(), sinstr.get_element_size(), sinstr.get_dest_node_id(), sinstr.get_message_id());
 
 	const auto allocation_base = allocations.at(sinstr.get_source_allocation_id());
